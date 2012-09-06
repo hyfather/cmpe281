08f703c490849ca933b7c17fc785bd8d943cc5ba
Nikhil Mungel <mail@hyfather.com>
2012-09-06 16:36:19 -0700

    Nikhil -- Initialized a rails app

---
+++ rest/mingle/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mingle::Application.config.secret_token = '2b449479b2cdbba3df458d9912d0e885d495c6b27ea6f5cd6934f58b263857a4cdb7b809abb53ecda2311b73c8ef9e28792136385d143fbb9be70e44ca23e873'
